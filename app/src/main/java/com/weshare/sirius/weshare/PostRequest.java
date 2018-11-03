4deb920ea9288ec388f1b9fdaf13ed5458e8feb2
Huiwen Yang <hw.yang@berkeley.edu>
2018-11-03 14:55:00 -0700

    add post request page

---
+++ app/src/main/java/com/weshare/sirius/weshare/PostRequest.java
@@ -0,0 +1,13 @@
+package com.weshare.sirius.weshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_request);
+    }
+}

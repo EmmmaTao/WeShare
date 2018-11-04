bea4281f727d486a79abae016f8cce058906b7f5
Huiwen Yang <hw.yang@berkeley.edu>
2018-11-04 03:15:51 -0800

    prepare to merge

--- app/src/main/java/com/weshare/sirius/weshare/FinishAcceptRequest.java
+++ app/src/main/java/com/weshare/sirius/weshare/FinishAcceptRequest.java
@@ -1,7 +1,10 @@
 package com.weshare.sirius.weshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FinishAcceptRequest extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class FinishAcceptRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish_accept_request);
+
+        Button FinishAcceptRequestBackToMainPageButton = (Button) findViewById(R.id.FinishAcceptRequestBackToMainPageButton);
+        FinishAcceptRequestBackToMainPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Back = new Intent(getApplicationContext(), WelcomePage.class);
+                startActivity(Back);
+            }
+        });
     }
 }

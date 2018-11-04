76e2079f71b83ce017ba6fdd4c3691730d8a661a
Huiwen Yang <hw.yang@berkeley.edu>
2018-11-03 21:10:17 -0700

    prepare for branch

--- app/src/main/java/com/weshare/sirius/weshare/RequestedItem.java
+++ app/src/main/java/com/weshare/sirius/weshare/RequestedItem.java
@@ -3,11 +3,11 @@ package com.weshare.sirius.weshare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RequestItemList extends AppCompatActivity {
+public class RequestedItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_request_item_list);
+        setContentView(R.layout.activity_requested_item);
     }
 }
